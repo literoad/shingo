d73a0d779c09dd1c6d6af59773f5fa60d21c431a
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-04-04 21:43:53 +0300

    build production images for amd64

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t cr.yandex/crprasudad2h8egspgoc/shingo:latest .
+docker build -t cr.yandex/crprasudad2h8egspgoc/shingo:latest --platform linux/amd64 .
 docker push cr.yandex/crprasudad2h8egspgoc/shingo:latest
